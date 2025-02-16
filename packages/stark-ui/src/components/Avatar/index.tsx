872fb29862bc45b079b7704a3b7fa87f5a707823
dokimion24 <leecy3610@gmail.com>
2025-02-16 18:12:41 +0900

    feat: Add Avatar component

---
+++ packages/stark-ui/src/components/Avatar/index.tsx
@@ -0,0 +1,134 @@
+import { cva } from '@styled-system/css';
+import type { ElementType, PropsWithChildren, ReactNode } from 'react';
+import { forwardRef } from 'react';
+import { User } from 'starkds-icons';
+
+import type {
+  PolymorphicComponentPropsWithoutRef,
+  PolymorphicComponentPropsWithRef,
+  PolymorphicRef,
+} from '@/types/polymorphic';
+
+export interface Props {
+  size?: 'sm' | 'md' | 'lg' | 'xl';
+  ImageComponent?: ElementType;
+  imageUrl?: string;
+  badgeIcon?: ReactNode;
+}
+
+type AvatarProps<T extends ElementType> = PolymorphicComponentPropsWithoutRef<T, Props> &
+  PropsWithChildren;
+
+type AvatarComponent = <T extends ElementType = 'div'>(
+  props: PolymorphicComponentPropsWithRef<T, AvatarProps<T>>,
+) => ReactNode;
+
+const AvatarContainerStyle = cva({
+  base: {
+    borderRadius: 'full',
+    borderWidth: '1px',
+    borderColor: 'border',
+    height: 'fit-content',
+    width: 'fit-content',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  variants: {
+    size: {
+      sm: { gap: '8px' },
+      md: { gap: '12px' },
+      lg: { gap: '16px' },
+      xl: { gap: '20px' },
+    },
+  },
+});
+
+const AvatarStyle = cva({
+  base: {
+    objectFit: 'cover',
+    borderRadius: 'full',
+    borderWidth: '1px',
+    borderColor: 'border',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  variants: {
+    size: {
+      sm: { width: '2rem', height: '2rem' },
+      md: { width: '2.5rem', height: '2.5rem' },
+      lg: { width: '3rem', height: '3rem' },
+      xl: { width: '3.5rem', height: '3.5rem' },
+    },
+  },
+});
+
+const AvatarIconStyle = cva({
+  variants: {
+    size: {
+      sm: { width: '1.25rem', height: '1.25rem' },
+      md: { width: '1.5rem', height: '1.5rem' },
+      lg: { width: '1.75rem', height: '1.75rem' },
+      xl: { width: '2rem', height: '2rem' },
+    },
+  },
+});
+
+const AvatarBadgeStyle = cva({
+  base: {
+    position: 'absolute',
+    bottom: '0',
+    right: '0',
+    bg: 'white',
+    borderRadius: 'full',
+    border: '1px solid',
+    borderColor: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  variants: {
+    size: {
+      sm: { width: '0.625rem', height: '0.625rem' },
+      md: { width: '0.75rem', height: '0.75rem' },
+      lg: { width: '0.875rem', height: '0.875rem' },
+      xl: { width: '1rem', height: '1rem' },
+    },
+  },
+});
+
+export const Avatar: AvatarComponent = forwardRef(
+  <T extends ElementType = 'div'>(
+    { as, size = 'lg', ImageComponent, imageUrl, badgeIcon, ...props }: AvatarProps<T>,
+    ref: PolymorphicRef<T>,
+  ) => {
+    const Component = as || 'div';
+
+    return (
+      <Component
+        ref={ref}
+        style={{ position: 'relative' }}
+        className={AvatarContainerStyle({
+          size,
+        })}
+        {...props}
+      >
+        {ImageComponent ? (
+          <ImageComponent className={AvatarStyle({ size })} />
+        ) : imageUrl ? (
+          <img alt="avatar" className={AvatarStyle({ size })} src={imageUrl} />
+        ) : (
+          <div aria-hidden={!!imageUrl} className={AvatarStyle({ size })}>
+            <User className={AvatarIconStyle({ size })} />
+          </div>
+        )}
+        {badgeIcon && <div className={AvatarBadgeStyle({ size })}>{badgeIcon}</div>}
+      </Component>
+    );
+  },
+);
+
+export default Avatar;
